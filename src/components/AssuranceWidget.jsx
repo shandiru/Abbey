e576496af320b915ff35407fe30b195f464ec71c
DirushanDev <rubakumardirushan@gmail.com>
2026-03-31 12:33:34 +0530

    add zindex

--- src/components/AssuranceWidget.jsx
+++ src/components/AssuranceWidget.jsx
@@ -5,6 +5,17 @@ export default function AssuranceWidget() {
     const script = document.createElement("script");
     script.src = "https://assurance.sysnetgs.com/assurancecard/be8e7fbc4499573f54aa6807a82bb2385a9e4a19f1db66f876e9fc528d1cd947/cardJs";
     script.async = true;
+
+    script.onload = () => {
+      // Script load ஆன உடனே duplicate cards remove பண்ணு
+      const widget = document.getElementById("widget-preview");
+      if (widget) {
+        while (widget.children.length > 1) {
+          widget.removeChild(widget.lastChild);
+        }
+      }
+    };
+
     document.body.appendChild(script);
 
     return () => {
